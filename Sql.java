3d6fb40660bc275beb295d384abd84653a409ea0
Kota Aoyama <cod14004@ict.nitech.ac.jp>
2026-05-19 16:20:21 +0900

    エラーを修正

--- Sql.java
+++ Sql.java
@@ -2,13 +2,17 @@ package prog4_jiyukadai;
 
 import java.sql.*;
 import java.util.*;
+
+import prog4_jiyukadai.Transaction.Category;
+import prog4_jiyukadai.Transaction.Type;
+
 import java.time.LocalDate;
 
-public class sql {
-    private static final String db = "accountBook";  //データベース名
-    private static final String table = "book";      //テーブル名
+public class Sql {
+    private static final String db = "pro4jiyukadai";  //データベース名
+    private static final String table = "test";      //テーブル名
     public static Connection con = null;
-    PreparedStatement pstmt = null;
+    public static PreparedStatement pstmt = null;
     
     public static void sqlInit() {
         try{
@@ -21,43 +25,59 @@ public class sql {
 
     //SQLのtableのデータ数を返す。
     public static int getDataNum() {
-        pstmt = con.prepareStatement("SELECT COUNT(*) FROM ?");
-        pstmt.setString(1,table);
-        ResuleSet rs = pstmt.executeQuery();
-        return rs.getInt(1);
+        try{
+            pstmt = con.prepareStatement("SELECT COUNT(*) FROM" + table);
+            ResultSet rs = pstmt.executeQuery();
+            return rs.getInt(1);
+        }catch(Exception e){
+            e.printStackTrace();
+            return 0;
+        }
+        
     }
     //SQLからデータを1つ取得し、Transactionインスタンスにして返す。
     public static Transaction readDB(int id) {
-        pstmt = con.prepareStatement("SELECT * FROM ? WHERE id = ?");
-        pstmt.setString(1,table);
-        pstmt.setInt(2,id);
-        ResultSet rs = pstmt.executeQuery();
-        if(rs.next()){
-            String categoryName = rs.getString("category");
-            Category category = Category.valueOf(categoryName);
-            int amount = rs.getInt("amount");
-            String typeName = rs.getString("type");
-            Type type = Type.valuof(typeName);
-            String dateText = rs.getString("date");
-            Localdate date = LocalDate.parse(dateText);
+        try{
+            pstmt = con.prepareStatement("SELECT * FROM ? WHERE id = ?");
+            pstmt.setString(1,table);
+            pstmt.setInt(2,id);
+            ResultSet rs = pstmt.executeQuery();
+            if(rs.next()){
+                String categoryName = rs.getString("category");
+                Category category = Category.valueOf(categoryName);
+                int amount = rs.getInt("amount");
+                String typeName = rs.getString("type");
+                TransactionKind type = TransactionKind.valueOf(typeName);
+                String dateText = rs.getString("date");
+                LocalDate date = LocalDate.parse(dateText);
 
-            return new Transaction(id,category,amount,type,date);
-        } else {
+                return new Transaction(id,category,amount,type,date);
+            } else {
+                return null;
+            }
+        }catch(Exception e){
+            e.printStackTrace();
             return null;
         }
+        
     }
 
     //SQLからデータをすべて取得し、TransactionのArrayListで返す。
-    public static ArraList<Transaction> readDBAll(){
-        ArrayList<Transaction> list = new ArraList<>();
+    public static ArrayList<Transaction> readDBAll(){
+        ArrayList<Transaction> list = new ArrayList<>();
         for(int id = 1; id < getDataNum(); id++) {
             list.add(readDB(id));
         }
         return list;
     }
 
-
-
-
+    //動作確認用//
+    public static void main(String[] args) {
+        sqlInit();
+        ArrayList<Transaction> list = readDBAll();
+        for(int i=0; i<list.size(); i++){
+            System.out.println(list.get(i));
+        }
+    }
 }
 //aaaaa
